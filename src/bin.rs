d01d245b63c4ce64be60935a4b26f07a46158c6c
jprio <jeanpierre.rio@gmail.com>
2024-05-26 15:12:03 +0200

    Routing policy

--- src/bin.rs
+++ src/bin.rs
@@ -5,6 +5,8 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use loadbalancer::Loadbalancer;
+    use loadbalancer::RoundRobinServerPolicy;
+    use loadbalancer::SingleServerPolicy;
     use reqwest::{Client, StatusCode};
     use wiremock::{matchers::method, Mock, MockServer, ResponseTemplate};
     #[tokio::test]
@@ -20,18 +22,77 @@ mod tests {
 
         let client = Client::new();
         println!("Mock server uri : {}", mock_server.uri());
+        let policy = Box::new(SingleServerPolicy::new(mock_server.uri().clone()));
+
         // The class under test, the load balancer itself
-        let server = Loadbalancer::new(8080, vec![mock_server.uri()]);
+        let server = Loadbalancer::new(8080, policy);
         let server_uri = server.uri();
         tokio::spawn(async move { server.run().await });
 
         // Wait for the server to be up (will fix this later)
-        tokio::time::sleep(std::time::Duration::from_secs(3)).await;
-
+        wait_server_up(&client, &server_uri, 3).await;
         // Check that we receive response from the mock backend
         // (and not from the load balancer)
         let response = client.get(server_uri).send().await.unwrap();
         assert_eq!(StatusCode::OK, response.status());
         assert_eq!("backend", response.text().await.unwrap());
+    }
+
+    #[tokio::test]
+    async fn test_get_round_robin_policy() {
+        let mocks = [
+            MockServer::start().await,
+            MockServer::start().await,
+            MockServer::start().await,
+        ];
+
+        Mock::given(method("GET"))
+            .respond_with(ResponseTemplate::new(200).set_body_string("1"))
+            .mount(&mocks[0])
+            .await;
+
+        Mock::given(method("GET"))
+            .respond_with(ResponseTemplate::new(200).set_body_string("2"))
+            .mount(&mocks[1])
+            .await;
+
+        Mock::given(method("GET"))
+            .respond_with(ResponseTemplate::new(200).set_body_string("3"))
+            .mount(&mocks[2])
+            .await;
+
+        let client = Client::new();
+        let mock_uris: Vec<_> = mocks.iter().map(|mock| mock.uri()).collect();
+
+        let policy = Box::new(RoundRobinServerPolicy::new(mock_uris.clone()));
+
+        // The class under test, the load balancer itself
+        let server = Loadbalancer::new(8082, policy);
+        let server_uri = server.uri();
+        tokio::spawn(async move { server.run().await });
+
+        // Wait for the server to be up (will fix this later)
+        wait_server_up(&client, &server_uri, 3).await;
+        // Check that we receive response from the mock backend
+        // (and not from the load balancer)
+        let response = client.get(&server_uri).send().await.unwrap();
+        assert_eq!(StatusCode::OK, response.status());
+        assert_eq!("1", response.text().await.unwrap());
+
+        let response = client.get(&server_uri).send().await.unwrap();
+        assert_eq!(StatusCode::OK, response.status());
+        assert_eq!("2", response.text().await.unwrap());
+    }
+
+    pub async fn wait_server_up(client: &Client, uri: &str, max_retries: usize) {
+        let health_uri = format!("{}/health", uri);
+        for _ in 0..max_retries {
+            let response = client.get(&health_uri).send().await;
+            if response.is_ok() {
+                return;
+            }
+            tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+        }
+        panic!("Server didn't start...");
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,94 @@
-use std::fmt::Display;
+use async_trait::async_trait;
+use std::{
+    collections::linked_list::Iter,
+    fmt::Display,
+    iter::Cycle,
+    sync::atomic::{AtomicUsize, Ordering},
+};
 
 use actix_web::{
     http::header::ContentType,
-    web::{self},
+    web::{self, Data},
     App, HttpRequest, HttpResponse, HttpServer, ResponseError,
 };
 use reqwest::Client;
 pub struct Loadbalancer {
     port: u16,
-    servers: Vec<String>,
+    data: Data<AppState>,
 }
 struct AppState {
+    client: Client,
+    policy: Box<SafeRoutingPolicy>,
+}
+pub type SafeRoutingPolicy = dyn RoutingPolicy + Sync + Send;
+
+pub struct SingleServerPolicy {
+    server: String,
+}
+
+impl SingleServerPolicy {
+    pub fn new(server: String) -> Self {
+        Self { server: server }
+    }
+}
+pub struct RoundRobinServerPolicy {
     servers: Vec<String>,
+    idx: AtomicUsize,
+}
+impl RoundRobinServerPolicy {
+    pub fn new(servers: Vec<String>) -> Self {
+        Self {
+            servers: servers,
+            idx: AtomicUsize::new(0),
+        }
+    }
+}
+#[async_trait]
+impl RoutingPolicy for RoundRobinServerPolicy {
+    async fn next(&self, request: &HttpRequest) -> String {
+        let servers = &self.servers;
+        let max_server_idx = servers.len() - 1;
+
+        // Update index
+        let idx = self
+            .idx
+            .fetch_update(Ordering::Relaxed, Ordering::Relaxed, |idx| match idx {
+                x if x >= max_server_idx => Some(0),
+                c => Some(c + 1),
+            })
+            .unwrap_or_default();
+
+        // Return next server to forward the request to
+        servers.get(idx).unwrap().clone()
+    }
+}
+
+#[async_trait]
+impl RoutingPolicy for SingleServerPolicy {
+    async fn next(&self, request: &HttpRequest) -> String {
+        self.server.clone()
+    }
 }
 impl Loadbalancer {
-    pub fn new(port: u16, uris: Vec<String>) -> Loadbalancer {
+    pub fn new(port: u16, policy: Box<SafeRoutingPolicy>) -> Self {
         Loadbalancer {
             port: port,
-            servers: uris,
+            data: web::Data::new(AppState {
+                client: Client::new(),
+                policy,
+            }),
         }
     }
     pub fn uri(&self) -> String {
         format!("http://127.0.0.1:{}", self.port)
     }
     pub async fn run(&self) {
-        let data = web::Data::new(AppState {
-            servers: self.servers.clone(),
-        });
+        let data = self.data.clone();
 
         HttpServer::new(move || {
             App::new()
+                // Healthcheck endpoint always returning 200 OK
+                .route("/health", web::get().to(HttpResponse::Ok))
                 .default_service(web::to(Self::handler))
                 // We add the initial instance of our shared app state
                 .app_data(data.clone())
@@ -40,16 +99,18 @@ impl Loadbalancer {
         .await
         .unwrap();
     }
+
     async fn handler(
         req: HttpRequest,
         data: web::Data<AppState>,
         bytes: web::Bytes,
     ) -> Result<HttpResponse, Error> {
-        let server = data.servers[0].clone();
+        let server = data.policy.next(&req).await;
+
         let uri = format!("{}{}", server, req.uri());
 
-        let client = Client::new();
-        let request_builder = client
+        let request_builder = data
+            .client
             .request(
                 reqwest::Method::from_bytes(req.method().clone().as_str().as_bytes()).unwrap(),
                 uri,
@@ -68,6 +129,10 @@ impl Loadbalancer {
         let body = response.bytes().await?;
         Ok(response_builder.body(body))
     }
+}
+#[async_trait]
+pub trait RoutingPolicy {
+    async fn next(&self, request: &HttpRequest) -> String;
 }
 
 #[derive(Debug)]
